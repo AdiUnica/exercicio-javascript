6b5ac9ec7346d3a34c0bb6923413744bac187821
Adi Cruz <adiunica@hotmail.com>
2023-05-17 14:13:17 -0300

    ola

--- facil10.js
+++ facil10.js
@@ -1,6 +1,6 @@
 /*  Faça um programa que pergunte em que turno você estuda. Peça para digitar M-matutino ou V-vespertino ou N-noturno. Imprima a mensagem “Bom dia!” ou  “Boa Noite” ou “Valor inválido”, conforme o caso.  */
 
- 
+  
 
 function verificarTurno() {
     var turno = document.getElementById("turno").value;
